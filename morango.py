970639588ce43bca41e23bad0ce8834b4d8dd711
matheusnassif <matheusnassifsgtau@gmail.com>
2023-05-12 15:22:28 -0300

    morango

---
+++ morango.py
@@ -0,0 +1,10 @@
+lado1=int(input())
+lado2=int(input())
+lado3=int(input())
+lado4=int(input())
+area= lado1 * lado2
+area1= lado3 * lado4
+if area>area1:
+    print(area)
+if area<area1:
+    print(area1)
